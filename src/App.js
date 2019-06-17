0a1e6c6e77cda82ca504cb32641092692d3f0a07
Ryan Kegerreis <ryankegerreis@icloud.com>
2019-06-17 17:32:06 -0400

    CORS issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component, Fragment } from "react";
 import Home from './Components/Home';
 import Form from './Components/Form'
-const API_KEY = "86af03787c0dd17e9115a463f68f4658"
+const API_KEY = "0ff1118f587bd7dd844a93c77e450e99"
 
 export default class App extends Component {
 
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(`https://openweathermap.org/data/2.5/weather?q=Miami&appid=${API_KEY}`);
     const data = await api_call.json();
+    console.log(data)
   }
 
 
@@ -15,7 +19,7 @@ export default class App extends Component {
     return (
       < Fragment >
         <Home />
-        <Form />
+        <Form getWeather={this.getWeather} />
 
 
 
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -5,7 +5,11 @@ export default class Form extends Component {
     return (
       <Fragment>
 
-        Form Test
+        <form onSubmit={this.props.getWeather}>
+          <input type="text" name="city" placeholder="City" />
+          <input type="text" name='country' placeholder='Country' />
+          <button>Get The Weather</button>
+        </form>
       </Fragment>
 
     )
